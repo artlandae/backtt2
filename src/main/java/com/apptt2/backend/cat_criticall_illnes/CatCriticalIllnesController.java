631aa2d450435a2c56f63816ea6b6f57a0a3e0ad
Arturo Landa Espinosa <artlandae@gmail.com>
2025-01-22 01:52:19 -0600

    consulta de alergias y enfermedades

--- src/main/java/com/apptt2/backend/cat_criticall_illnes/CatCriticalIllnesController.java
+++ src/main/java/com/apptt2/backend/cat_criticall_illnes/CatCriticalIllnesController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin()
 @RequestMapping("/api/cat-critical-illnes")
 public class CatCriticalIllnesController {
 
